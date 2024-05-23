1b6b1e292d1495687a8dca19f57e43b52de0b08e
DanielleInkster <danielle.inkster@gmail.com>
2024-05-23 14:45:49 +0100

    number of one bits

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,9 @@
 package org.practice.datastructuresalgorithms
 
-import org.practice.datastructuresalgorithms.Arrays.threeSums
+import org.practice.datastructuresalgorithms.Binary.countSetBits
 
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val nums = intArrayOf(-1, 0, 1, 2, -1, -4)
-
-    threeSums(nums)
+    countSetBits(11)
 }
